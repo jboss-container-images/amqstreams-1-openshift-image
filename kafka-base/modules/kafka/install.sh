11511cc116462fbcad27b427fbf7889c91910512
Paolo Patierno <ppatierno@live.com>
2018-04-06 12:45:45 +0200

    Updated jboss user permissions on the Kafka dir Added running Kafka and Zookeeper as user jboss (185)

--- kafka-base/modules/kafka/install.sh
+++ kafka-base/modules/kafka/install.sh
@@ -10,3 +10,6 @@ tar xvfz "${SOURCES_DIR}/kafka.tar.gz" -C ${KAFKA_HOME} --strip-components=1
 mkdir /opt/prometheus
 mkdir /opt/prometheus/config
 mv ${SOURCES_DIR}/jmx_prometheus_javaagent.jar /opt/prometheus/jmx_prometheus_javaagent.jar
+
+chown -R jboss:root ${KAFKA_HOME}/bin
+chmod 0755 ${KAFKA_HOME}/bin

f0227519bb4e3339546970d6e38deec2d06094b2
Kyle Liberti <kliberti@redhat.com>
2018-08-17 15:50:38 -0400

    Update Kafka/Kafka-connect scripts

--- kafka/modules/kafka/install.sh
+++ kafka/modules/kafka/install.sh
@@ -4,11 +4,7 @@ set -e
 
 SCRIPT_DIR=$(dirname $0)
 ADDED_DIR=${SCRIPT_DIR}/added
-CONFIG_DIR=${ADDED_DIR}/config
 SCRIPTS_DIR=${ADDED_DIR}/scripts
-
-# copy configuration files
-cp -r ${CONFIG_DIR}/* ${KAFKA_HOME}/config/
 
 # copy scripts for starting Kafka
 cp -r ${SCRIPTS_DIR}/* ${KAFKA_HOME}/

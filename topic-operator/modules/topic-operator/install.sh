852952b3c227b2e44d5adcb228057892617b5d6f
Kyle Liberti <kliberti@redhat.com>
2018-08-23 11:16:52 -0400

    Backporting enhanced Topic Operator scripts

--- topic-operator/modules/topic-operator/install.sh
+++ topic-operator/modules/topic-operator/install.sh
@@ -3,5 +3,11 @@
 set -e
 
 SOURCES_DIR=/tmp/artifacts
+SCRIPT_DIR=$(dirname $0)
+ADDED_DIR=${SCRIPT_DIR}/added
+SCRIPTS_DIR=${ADDED_DIR}/scripts
+
+cp -r ${SCRIPTS_DIR}/* /bin/
+chmod -R 755 /bin/
 
 mv "${SOURCES_DIR}/topic-operator.jar" /

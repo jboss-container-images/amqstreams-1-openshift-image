acb950267e353fa63ba272fffd0c7c77ab3a2f81
Kyle Liberti <kliberti@redhat.com>
2019-02-28 21:28:53 -0500

    Adding license extractor scripts

--- java-base/modules/os-javabase-install/install.sh
+++ java-base/modules/os-javabase-install/install.sh
@@ -3,6 +3,12 @@ set -e
 
 SCRIPT_DIR=$(dirname $0)
 SCRIPTS_DIR=${SCRIPT_DIR}/scripts
+SOURCES_DIR=/tmp/artifacts
+LICENSE_DIR=/root/licenses
+
+mkdir $LICENSE_DIR
+mv ${SOURCES_DIR}/* ${LICENSE_DIR}
+chmod -R 755 ${LICENSE_DIR}
 
 mv ${SCRIPTS_DIR}/launch_java.sh /bin
 
--- topic-operator/modules/topic-operator/install.sh
+++ topic-operator/modules/topic-operator/install.sh
@@ -5,6 +5,11 @@ set -e
 SOURCES_DIR=/tmp/artifacts
 SCRIPT_DIR=$(dirname $0)
 SCRIPTS_DIR=${SCRIPT_DIR}/scripts
+LICENSE_DIR=/root/licenses
+PRODUCT_LICENSE_DIR=${LICENSE_DIR}/${COM_REDHAT_COMPONENT}
+
+mkdir -p ${PRODUCT_LICENSE_DIR}
+tar -xvzf ${LICENSE_DIR}/*.tgz -C ${PRODUCT_LICENSE_DIR}
 
 cp -r ${SCRIPTS_DIR}/* /bin/
 chmod -R 755 /bin/

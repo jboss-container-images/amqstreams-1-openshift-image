7d2089bdb5a0f4d6b0c5500ebd2ecf01afe2d709
Kyle Liberti <kliberti@redhat.com>
2019-03-23 19:09:59 -0400

    Enhancements + Refactor for chain automation for 1.1.0 release

--- chain/build.py
+++ chain/build.py
@@ -4,6 +4,8 @@ import threading
 import time
 import traceback
 
+from chain.logging import log_build_stdout
+
 class Build(threading.Thread):
   """ 
   Thread object which executes and tracks a forked process. 
@@ -14,66 +16,65 @@ class Build(threading.Thread):
     cmd:        command to run in every image directory
     lock:       mutex for resources shared among builds (e.g. dist-git repo)
   """
-  def __init__(self, image, cmd, lock):
-    self.image = image
-    self.cmd = cmd
-    self.lock = lock
-    self.failure = None
+  def __init__(self, image, config, lock):
+    self.image       = image
+    self.config      = config 
+    self.log         = image.log
+    self.chain_log   = config.log 
+    self.lock        = lock
+    self.failure     = None
+     
     threading.Thread.__init__(self)
 
   def run(self):
     stdout=""
-    cwd = os.path.join(os.getcwd(), self.image)
+    cwd = self.image.path
+    if(self.config.build_type == "release" or self.config.build_type == "osbs"):
+        self.config.cmd = self.config.cmd + " --build-osbs-target " + "rh-amqstreams-1.1-rhel-7-containers-candidate" #self.image.target
+        #print(self.config.cmd)
     if(os.path.exists(cwd)):
-      self.thread_safe_print("Building %s ..." % self.image)
+      self.thread_safe_print("Building %s ..." % self.image.tree_name)
       try:
         self.lock.acquire()
-        self.log_build(self.cmd + "\n")
-        process = subprocess.Popen(self.cmd.split(),
+        log_build_stdout(self.config.cmd + "\n", self.image)
+        process = subprocess.Popen(self.config.cmd.split(),
                            cwd=cwd,
                            shell=False,
                            stdout=subprocess.PIPE,
                            stderr=subprocess.STDOUT,
-                           stdin=subprocess.PIPE)
+                           stdin=subprocess.PIPE,
+                           universal_newlines=True)
 
         self.commit_and_kick(process)
         self.lock.release()
 
         stdout, err  = process.communicate()
         self.failure = process.returncode
-        self.log_build(stdout)
+        log_build_stdout(stdout, self.image)
         if(not self.failure):
-          self.thread_safe_print("Completed " + self.image)
+          self.thread_safe_print("Completed " + self.image.tree_name)
       except Exception as e:
-        self.log_build(stdout + traceback.format_exc())
+        log_build_stdout(stdout + traceback.format_exc(), self.image)
         self.lock.release()
         self.failure = process.returncode
-        self.thread_safe_print("Incomplete " + self.image)
+        self.thread_safe_print("Incomplete " + self.image.tree_name)
 
   def commit_and_kick(self, process):
     ''' Commits code to dist-git and kicks OSBS build '''
-    if("osbs" in self.cmd):
+    if(self.config.osbs_build):
       text = ""
       while( process.poll() is None ):
         output = process.stdout.readline()
         text += output
         # If prompted, answer yes
-        process.stdin.write(b'Y\n')
+        process.stdin.write('Y\n')
+        process.stdin.flush()
         if("Created task" in output):
           break
-      self.log_build(text)
+      log_build_stdout(text, self.image)
  
   def thread_safe_print(self, str):
-    ''' Allow only one thread to write to stdout at a time'''
+    ''' Allow only one thread to write to stdout at a time '''
     self.lock.acquire()
     print(str)
     self.lock.release()
-
-  def log_build(self, text):
-    ''' Write build stdout to log file '''
-    logs_dir="logs"
-    if not os.path.exists(logs_dir):
-      os.makedirs(logs_dir)
-    log_file = os.path.join(logs_dir, self.image + ".log")
-    with open(log_file, 'a') as f:
-      f.write(str(text))
--- chain/hierarchy.py
+++ chain/hierarchy.py
@@ -1,73 +1,143 @@
 import os
-from chain.traverse import traverse, get_image_name, get_image_base
+import copy
+
+from chain.traverse import traverse, get_images
+from chain.image import Image
+
+def print_tree(d, indent=0):
+    ''' Prints formatted nested dictionary '''
+    for key, value in d.items():
+        print('  ' * indent + str(key.tree_name))
+        if isinstance(value, dict):
+            print_tree(value, indent+1)
 
 def rank_to_hierarchy_dict(image_list):
-  ''' Given 2D list of [basename, imagename] pairs, 
-      build and return hierarchical dict '''
-  h_dict = {}
+    ''' Given Image objects, build and return
+        a hierarchical dict '''
+    h_dict = {}
 
-  for image in image_list:
-    if(not hierarchy(image, h_dict)):
-      h_dict.update( { image[0] : { image[1] : {} } } )
-
-  return h_dict
-
-def rank(image_list, image):
- ''' Returns number of images the given image is based upon '''
- for n, b in image_list:
-   if(image == b):
-     return rank(image_list, n) + 1
- return 1
-
-def prune_dict(image, matrix):
-  ''' Returns dependency tree of specified image '''
-  for k,v in matrix.items():
-    if(image == k):
-      return {k : v}
-    else:
-      dep_tree = prune_dict(image, v)
-      if(dep_tree):
-        return dep_tree
-
-def count_images(h_dict):
-  ''' Returns number of images in hierarchical dictionary '''
-  count = 0
-  for k,v in h_dict.items():
-    if(v == {}):
-      count += 1
-    else:
-      count += count_images(v) + 1
-
-  return count
+    for image in image_list:
+        if(not hierarchy(image, h_dict)):
+            h_dict.update( { image : {} } )
+    return h_dict
 
 def hierarchy(image, h_dict):
-  ''' Organizes image into a nested hierarchical dictionary '''
-  for k,v in h_dict.items():
-    if(image[0] == k):
-      h_dict[k].update( { image[1] : {} } )
-      return True
-    else:
-      if(hierarchy(image, v)):
-        return True
+    ''' Organizes image into a nested hierarchical dictionary '''
+    for k,v in h_dict.items():
+        if(image.tree_base == k.tree_name):
+            image.parent_image = k
+            h_dict[k].update( { image : {} } )
+            return True
+        else:
+            if(hierarchy(image, v)):
+                return True
 
-def create_hierarchy_dict(build_image):
-  ''' Returns hierarchical dictionary '''
-  image_list = [] 
-  rank_list = []
+def rank(image_list, image):
+   ''' Returns number of images the given image is based upon '''
+   for i in image_list:
+       if(image == i.tree_base):
+           return rank(image_list, i.tree_name) + 1
+   return 1
 
-  traverse(os.getcwd(), get_image_name, image_list)
+def prune_dict(image, h_dict, build_children_only):
+    ''' Returns dependency tree of specified image '''
+    for k,v in h_dict.items():
+      if(image == k.tree_name or (len(image.split(":")) < 2 and 
+          image.split(":")[0] == k.tree_name.split(":")[0]) ):
+          if(build_children_only):
+              return v
+          else:
+              return {k : v}
+      else:
+          dep_tree = prune_dict(image, v, build_children_only)
+          if(dep_tree):
+            return dep_tree
 
-  # Traverse filesystem, collecting images name/base
-  traverse(os.getcwd(), get_image_base, image_list, rank_list)
+def count_images(h_dict):
+    ''' Returns number of images in hierarchical dictionary '''
+    count = 0
+    for k,v in h_dict.items():
+        if(v == {}):
+            count += 1
+        else:
+            count += count_images(v) + 1
+    return count
 
-  # Sort images by rank ( number of images layered ontop of )
-  rank_list = sorted(rank_list, key=lambda image:rank(rank_list, image[0]))
+def flatten_hierarchy_dict(h_dict, flat=[]):
+    ''' Converts multi-dimensional image dict to image list'''
+    for k,v in h_dict.items():
+        flat.append(k)
+        flatten_hierarchy_dict(v, flat)
+    return flat
 
-  # Create hierarchical dictionary based on rank
-  h_dict = rank_to_hierarchy_dict(rank_list)
+def add_multi_jar_images(base, jar_data, h_dict):
+    ''' Refactor later '''
+    n_dict = {}
+    if isinstance(h_dict, dict): 
+        for image, dep_images in h_dict.items():
+            name = lambda x: x.split(":")[0]
+            if(base is None or name(base) == name(image.tree_name)):
+                for v, jars in jar_data.items():
+                    versioned_image = copy.deepcopy(image)
+                    versioned_image.tree_name = image.tree_name + "-" + v
+                    versioned_image.path = os.path.join(versioned_image.path, v)
+                    versioned_image.full_path = os.path.join(versioned_image.path, "image.yaml")
+                    versioned_image.multi_version = True
+                    versioned_image.kafka_version = v
+                    versioned_image.kafka_version_number = v.split("-")[-1]
+                    if(base):
+                        versioned_image.jar = jars
+                    else:
+                        versioned_image.parent_image.multi_version = True
+                    n_dict.update( { versioned_image : add_multi_jar_images(None, { v : jars }, dep_images) } ) 
+            else:
+                n_dict.update({ image : add_multi_jar_images(base, jar_data, dep_images) })
+    return n_dict
 
-  if build_image is not None:
-    # Prune dictionary to only specified image tree  
-    h_dict = prune_dict(build_image, h_dict)
+def add_trunk_images(h_dict, config):
+    ''' Refactor later '''
+    trunks = {}
+    for image in h_dict.items():
+        if(image[0].base in [trunk.name for trunk in trunks.keys()]):
+            for trunk in trunks.keys():
+                if(image[0].base == trunk.name):
+                  image[0].parent_image = trunk
+                  trunks[trunk].update(  { image[0] : image[1]  }  )
+        else:
+            trunk = Image(".", config)
+            trunk.name      = image[0].base
+            trunk.tree_name = image[0].base 
+            trunk.trunk_image = True
+            image[0].parent_image = trunk
+            trunks.update( { trunk : { image[0] : image[1]  } } )
+      
+    return trunks
 
-  return h_dict
+def create_hierarchy_dict(config):
+    ''' Returns hierarchical dictionary '''
+    image_list = [] 
+   
+    # Traverse filesystem, collecting image attributes
+    traverse(os.getcwd(), get_images, image_list, config)
+ 
+    image_list = sorted(image_list, key=lambda image:rank(image_list, image.tree_base), reverse=True)
+
+    # Create hierarchical dictionary based on rank
+    h_dict = rank_to_hierarchy_dict(image_list)
+  
+    # Get multi-version image information
+    for image, jar_tag in config.jars.items():
+        if(jar_tag.multi_version):
+            h_dict = add_multi_jar_images(image, jar_tag.combination, h_dict)
+ 
+    h_dict = add_trunk_images(h_dict, config)
+    if(config.build_image is not None):
+        # Prune dictionary to only specified image tree
+        h_dict = prune_dict(config.build_image, h_dict, config.build_children_only)
+  
+    if(not config.build_children):
+        h_dict = dict([ (k,{}) for k,v in h_dict.items() ])
+
+    print_tree(h_dict)
+
+    return h_dict

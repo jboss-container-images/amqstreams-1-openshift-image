fa41ca5f4e56e4246e743287a7082de8945fe9ed
Stanislav Knot <sknot@redhat.com>
2019-06-06 10:14:56 +0200

    copy manifests to the root

--- operator/modules/olm/install.sh
+++ operator/modules/olm/install.sh
@@ -5,9 +5,7 @@ SCRIPT_DIR=$(dirname $0)
 MANIFESTS_DIR=${SCRIPT_DIR}/manifests
 
 # create destination folder of manifests
-mkdir -p ${STRIMZI_HOME}/manifests
+mkdir -p /manifests
 
 # copy manifests files
-cp -r ${MANIFESTS_DIR}/* ${STRIMZI_HOME}/manifests
-
-chmod -R 0755 ${STRIMZI_HOME}/manifests
+cp -r ${MANIFESTS_DIR}/* /manifests

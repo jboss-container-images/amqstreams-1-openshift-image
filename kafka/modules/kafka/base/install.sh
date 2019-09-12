2e1e0487d02d10399636a69895d47541b5a419a5
Paolo Patierno <ppatierno@live.com>
2019-09-12 18:15:03 +0200

    Added Kafka exporter

--- kafka/modules/kafka/base/install.sh
+++ kafka/modules/kafka/base/install.sh
@@ -7,10 +7,12 @@ SCRIPT_DIR=$(dirname $0)
 SCRIPTS_DIR=${SCRIPT_DIR}/scripts
 STUNNEL_DIR=${SCRIPT_DIR}/stunnel-scripts
 S2I_DIR=${SCRIPT_DIR}/s2i-scripts
+EXPORTER_DIR=${SCRIPT_DIR}/exporter-scripts
 
 mkdir -p $KAFKA_HOME/libs/
 mkdir $STUNNEL_HOME
 mkdir $S2I_HOME
+mkdir $KAFKA_EXPORTER_HOME
 mkdir -p -m g+rw /usr/local/var/run/
 
 cp -r ${SOURCES_DIR}/jmx_prometheus_javaagent.jar ${KAFKA_HOME}/libs/
@@ -22,6 +24,8 @@ cp -r ${SOURCES_DIR}/opentracing-api.jar ${KAFKA_HOME}/libs/
 cp -r ${SOURCES_DIR}/opentracing-util.jar ${KAFKA_HOME}/libs/
 cp -r ${SOURCES_DIR}/opentracing-kafka-client.jar ${KAFKA_HOME}/libs/
 
+tar xvfz "${SOURCES_DIR}/kafka-exporter.tar.gz" -C ${KAFKA_EXPORTER_HOME} --strip-components=1
+
 chmod -R 0755 ${KAFKA_HOME}
 
 cp -r ${SCRIPTS_DIR}/* ${KAFKA_HOME}/
@@ -32,3 +36,6 @@ chmod -R 755 ${STUNNEL_HOME}
 
 cp -r ${S2I_DIR}/* ${S2I_HOME}/
 chmod -R 755 ${S2I_HOME}
+
+cp -r ${EXPORTER_DIR}/* ${KAFKA_EXPORTER_HOME}/
+chmod -R 755 ${KAFKA_EXPORTER_HOME}

9f73e7110f842cdb79e8097608f56e832303c0b7
Paolo Patierno <ppatierno@live.com>
2019-09-12 20:48:24 +0200

    Fixed comment

--- kafka/modules/kafka/base/install.sh
+++ kafka/modules/kafka/base/install.sh
@@ -24,7 +24,7 @@ cp -r ${SOURCES_DIR}/opentracing-api.jar ${KAFKA_HOME}/libs/
 cp -r ${SOURCES_DIR}/opentracing-util.jar ${KAFKA_HOME}/libs/
 cp -r ${SOURCES_DIR}/opentracing-kafka-client.jar ${KAFKA_HOME}/libs/
 
-unzip -qo ${SOURCES_DIR}/kafka-exporter.zip -d ${KAFKA_EXPORTER_HOME}/
+unzip -qo ${SOURCES_DIR}/kafka_exporter.zip -d ${KAFKA_EXPORTER_HOME}/
 
 chmod -R 0755 ${KAFKA_HOME}
 

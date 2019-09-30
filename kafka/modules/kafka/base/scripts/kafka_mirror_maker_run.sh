77dcff970c0e43bdd140f18460894334812d303c
Paolo Patierno <ppatierno@live.com>
2019-09-30 17:01:39 +0200

    Moving to AMQ Streams 1.3 (#124)
    * Added new artifacts
    Updated scripts
    Updated to version 1.3
    
    * Updated md5 for manually cached artifacts from Strimzi master
    
    * Added Kafka exporter
    
    * Updated with the first productized kafka exporter artifact
    
    * Removed artifacts uploaded by mistake
    
    * Fixed comment
    
    * Moved copy of the opentracing client library
    
    * Updated Kafka scripts with OAuth support
    
    * Updated Kafka scripts with fixed bug
    
    * Script updates for RC1 (#128)
    
    * Adding wildcard to jar names used in image scripts
    
    * Alter scripts to handle Kafka zips
    
    * Update MD5s for RC1

--- kafka/modules/kafka/base/scripts/kafka_mirror_maker_run.sh
+++ kafka/modules/kafka/base/scripts/kafka_mirror_maker_run.sh
@@ -1,39 +1,42 @@
 #!/usr/bin/env bash
 set +x
 
-if [ -n "$KAFKA_MIRRORMAKER_TRUSTED_CERTS_CONSUMER" ] || [ -n "$KAFKA_MIRRORMAKER_TRUSTED_CERTS_PRODUCER" ]; then
-    # Generate temporary keystore password
-    export CERTS_STORE_PASSWORD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32)
+# Generate temporary keystore password
+export CERTS_STORE_PASSWORD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32)
 
-    mkdir -p /tmp/kafka
+# Create dir where keystores and truststores will be stored
+mkdir -p /tmp/kafka
 
-    # Import certificates into keystore and truststore
-    # $1 = trusted certs, $2 = TLS auth cert, $3 = TLS auth key, $4 = truststore path, $5 = keystore path, $6 = certs and key path
-    ./kafka_mirror_maker_tls_prepare_certificates.sh \
-        "$KAFKA_MIRRORMAKER_TRUSTED_CERTS_CONSUMER" \
-        "$KAFKA_MIRRORMAKER_TLS_AUTH_CERT_CONSUMER" \
-        "$KAFKA_MIRRORMAKER_TLS_AUTH_KEY_CONSUMER" \
-        "/tmp/kafka/consumer.truststore.p12" \
-        "/tmp/kafka/consumer.keystore.p12" \
-        "/opt/kafka/consumer-certs"
+# Import certificates into keystore and truststore
+# $1 = trusted certs, $2 = TLS auth cert, $3 = TLS auth key, $4 = truststore path, $5 = keystore path, $6 = certs and key path
+./kafka_mirror_maker_tls_prepare_certificates.sh \
+    "$KAFKA_MIRRORMAKER_TRUSTED_CERTS_CONSUMER" \
+    "$KAFKA_MIRRORMAKER_TLS_AUTH_CERT_CONSUMER" \
+    "$KAFKA_MIRRORMAKER_TLS_AUTH_KEY_CONSUMER" \
+    "/tmp/kafka/consumer.truststore.p12" \
+    "/tmp/kafka/consumer.keystore.p12" \
+    "/opt/kafka/consumer-certs" \
+    "/opt/kafka/consumer-oauth-certs" \
+    "/tmp/kafka/consumer-oauth.keystore.p12"
 
-    ./kafka_mirror_maker_tls_prepare_certificates.sh \
-        "$KAFKA_MIRRORMAKER_TRUSTED_CERTS_PRODUCER" \
-        "$KAFKA_MIRRORMAKER_TLS_AUTH_CERT_PRODUCER" \
-        "$KAFKA_MIRRORMAKER_TLS_AUTH_KEY_PRODUCER" \
-        "/tmp/kafka/producer.truststore.p12" \
-        "/tmp/kafka/producer.keystore.p12" \
-        "/opt/kafka/producer-certs"
-fi
+./kafka_mirror_maker_tls_prepare_certificates.sh \
+    "$KAFKA_MIRRORMAKER_TRUSTED_CERTS_PRODUCER" \
+    "$KAFKA_MIRRORMAKER_TLS_AUTH_CERT_PRODUCER" \
+    "$KAFKA_MIRRORMAKER_TLS_AUTH_KEY_PRODUCER" \
+    "/tmp/kafka/producer.truststore.p12" \
+    "/tmp/kafka/producer.keystore.p12" \
+    "/opt/kafka/producer-certs" \
+    "/opt/kafka/producer-oauth-certs" \
+    "/tmp/kafka/producer-oauth.keystore.p12"
 
 # Generate and print the consumer config file
 echo "Kafka Mirror Maker consumer configuration:"
-./kafka_mirror_maker_consumer_config_generator.sh | tee /tmp/strimzi-consumer.properties
+./kafka_mirror_maker_consumer_config_generator.sh | tee /tmp/strimzi-consumer.properties | sed 's/sasl.jaas.config=.*/sasl.jaas.config=[hidden]/g'
 echo ""
 
 # Generate and print the producer config file
 echo "Kafka Mirror Maker producer configuration:"
-./kafka_mirror_maker_producer_config_generator.sh | tee /tmp/strimzi-producer.properties
+./kafka_mirror_maker_producer_config_generator.sh | tee /tmp/strimzi-producer.properties | sed 's/sasl.jaas.config=.*/sasl.jaas.config=[hidden]/g'
 echo ""
 
 # Disable Kafka's GC logging (which logs to a file)...
@@ -47,9 +50,18 @@ fi
 # directory avoids trying to create it (and logging a permission denied error)
 export LOG_DIR="$KAFKA_HOME"
 
+# Enabling the Mirror Maker agent which monitors readiness / liveness
+rm /tmp/mirror-maker-ready /tmp/mirror-maker-alive 2> /dev/null
+export KAFKA_OPTS="-javaagent:$(ls $KAFKA_HOME/libs/mirror-maker-agent*.jar)=/tmp/mirror-maker-ready:/tmp/mirror-maker-alive:${STRIMZI_READINESS_PERIOD:-10}:${STRIMZI_LIVENESS_PERIOD:-10}"
+
 # enabling Prometheus JMX exporter as Java agent
 if [ "$KAFKA_MIRRORMAKER_METRICS_ENABLED" = "true" ]; then
-  export KAFKA_OPTS="-javaagent:/opt/prometheus/jmx_prometheus_javaagent.jar=9404:$KAFKA_HOME/custom-config/metrics-config.yml"
+  export KAFKA_OPTS="$KAFKA_OPTS -javaagent:$(ls $KAFKA_HOME/libs/jmx_prometheus_javaagent*.jar)=9404:$KAFKA_HOME/custom-config/metrics-config.yml"
+fi
+
+# enabling Tracing agent (initializes Jaeger tracing) as Java agent
+if [ "$STRIMZI_TRACING" = "jaeger" ]; then
+  export KAFKA_OPTS="$KAFKA_OPTS -javaagent:$(ls $KAFKA_HOME/libs/tracing-agent*.jar)=jaeger"
 fi
 
 if [ -z "$KAFKA_HEAP_OPTS" -a -n "${DYNAMIC_HEAP_FRACTION}" ]; then
@@ -71,6 +83,14 @@ if [ -n "$KAFKA_MIRRORMAKER_NUMSTREAMS" ]; then
     numstreams="--num.streams ${KAFKA_MIRRORMAKER_NUMSTREAMS}"
 fi
 
+if [ -n "$KAFKA_MIRRORMAKER_OFFSET_COMMIT_INTERVAL" ]; then
+    offset_commit_interval="--offset.commit.interval.ms $KAFKA_MIRRORMAKER_OFFSET_COMMIT_INTERVAL"
+fi
+
+if [ -n "$KAFKA_MIRRORMAKER_ABORT_ON_SEND_FAILURE" ]; then
+    abort_on_send_failure="--abort.on.send.failure $KAFKA_MIRRORMAKER_ABORT_ON_SEND_FAILURE"
+fi
+
 . ./set_kafka_gc_options.sh
 
 # starting Kafka Mirror Maker with final configuration
@@ -78,4 +98,6 @@ exec $KAFKA_HOME/bin/kafka-mirror-maker.sh \
 --consumer.config /tmp/strimzi-consumer.properties \
 --producer.config /tmp/strimzi-producer.properties \
 $whitelist \
-$numstreams
+$numstreams \
+$offset_commit_interval \
+$abort_on_send_failure

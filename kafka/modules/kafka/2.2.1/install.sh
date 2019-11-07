93b0ec3172d5f683ee7f8909abe5ccf3fa37dd48
kyguy <kliberti@redhat.com>
2019-11-07 14:59:50 -0500

    Change kafka.jar names to enable CPaaS processing (#140)

--- kafka/modules/kafka/2.2.1/install.sh
+++ kafka/modules/kafka/2.2.1/install.sh
@@ -7,8 +7,8 @@ LICENSE_DIR=/root/licenses
 PRODUCT_LICENSE_DIR=${LICENSE_DIR}/${COM_REDHAT_COMPONENT}
 
 # Copy contents of zip without the root dir
-TMP=$(zipinfo -1  ${SOURCES_DIR}/kafka.zip | grep -oE '^[^/]+' | uniq)
-unzip ${SOURCES_DIR}/kafka.zip
+TMP=$(zipinfo -1  ${SOURCES_DIR}/kafka-*.zip | grep -oE '^[^/]+' | uniq)
+unzip ${SOURCES_DIR}/kafka-*.zip
 mv ${TMP}/* ${KAFKA_HOME}/
 
 cp -r ${KAFKA_HOME}/bin/kafka_exporter ${KAFKA_EXPORTER_HOME}/

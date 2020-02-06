c741da1df1d9c54030244a974790fb04added768
Paolo Patierno <ppatierno@live.com>
2020-02-06 15:10:19 +0100

    Backported Kafka scripts

--- kafka/modules/kafka/base/scripts/kafka_tls_prepare_certificates.sh
+++ kafka/modules/kafka/base/scripts/kafka_tls_prepare_certificates.sh
@@ -20,6 +20,16 @@ function create_keystore {
    RANDFILE=/tmp/.rnd openssl pkcs12 -export -in $3 -inkey $4 -chain -CAfile $5 -name $6 -password pass:$2 -out $1
 }
 
+# Parameters:
+# $1: Path to the new keystore
+# $2: Truststore password
+# $3: Public key to be imported
+# $4: Private key to be imported
+# $5: Alias of the certificate
+function create_keystore_without_ca_file {
+   RANDFILE=/tmp/.rnd openssl pkcs12 -export -in $3 -inkey $4 -name $5 -password pass:$2 -out $1
+}
+
 # Searches the directory with the CAs and finds the CA matching our key.
 # This is useful during certificate renewals
 #
@@ -50,7 +60,8 @@ echo "Looking for the right CA"
 CA=$(find_ca /opt/kafka/cluster-ca-certs /opt/kafka/broker-certs/$HOSTNAME.crt)
 
 if [ ! -f "$CA" ]; then
-    echo "No CA found. This exiting."
+    echo "No CA found. Thus exiting."
+    exit 1
 fi
 echo "Found the right CA: $CA"
 
@@ -62,6 +73,20 @@ create_keystore /tmp/kafka/cluster.keystore.p12 $CERTS_STORE_PASSWORD \
     $HOSTNAME
 echo "Preparing keystore for replication and clienttls listener is complete"
 
+CUSTOM_CERT_DIR="/opt/kafka/certificates/custom-tls-9093-certs"
+if [ -d "$CUSTOM_CERT_DIR" ]; then
+    echo "Preparing custom keystore for tls listener"
+    create_keystore_without_ca_file /tmp/kafka/custom-tls-9093.keystore.p12 $CERTS_STORE_PASSWORD ${CUSTOM_CERT_DIR}/tls.crt ${CUSTOM_CERT_DIR}/tls.key custom-key
+    echo "Preparing custom keystore for tls listener is complete"
+fi
+
+CUSTOM_CERT_DIR="/opt/kafka/certificates/custom-external-9094-certs"
+if [ -d "$CUSTOM_CERT_DIR" ]; then
+    echo "Preparing custom keystore for external listener"
+    create_keystore_without_ca_file /tmp/kafka/custom-external-9094.keystore.p12 $CERTS_STORE_PASSWORD ${CUSTOM_CERT_DIR}/tls.crt ${CUSTOM_CERT_DIR}/tls.key custom-key
+    echo "Preparing custom keystore for external listener is complete"
+fi
+
 echo "Preparing truststore for clienttls listener"
 # Add each certificate to the trust store
 STORE=/tmp/kafka/clients.truststore.p12
@@ -72,44 +97,66 @@ for CRT in /opt/kafka/client-ca-certs/*.crt; do
 done
 echo "Preparing truststore for clienttls listener is complete"
 
-if [ -d /opt/kafka/oauth-certs/client ]; then
-  echo "Preparing truststore for OAuth on plain listener"
+OAUTH_CERT_DIR="/opt/kafka/certificates/oauth-plain-9092-certs"
+OAUTH_STORE="/tmp/kafka/oauth-plain-9092.truststore.p12"
+if [ -d "$OAUTH_CERT_DIR" ]; then
+  echo "Preparing truststore for OAuth on PLAIN listener"
+
   # Add each certificate to the trust store
-  STORE=/tmp/kafka/oauth-client.truststore.p12
   declare -i INDEX=0
-  for CRT in /opt/kafka/oauth-certs/client/**/*; do
+  for CRT in $OAUTH_CERT_DIR/**/*; do
     ALIAS="oauth-${INDEX}"
-    echo "Adding $CRT to truststore $STORE with alias $ALIAS"
-    create_truststore "$STORE" "$CERTS_STORE_PASSWORD" "$CRT" "$ALIAS"
+    echo "Adding $CRT to truststore $OAUTH_STORE with alias $ALIAS"
+    create_truststore "$OAUTH_STORE" "$CERTS_STORE_PASSWORD" "$CRT" "$ALIAS"
     INDEX+=1
   done
-  echo "Preparing truststore for OAuth on plain listener is complete"
+  echo "Preparing truststore for OAuth on PLAIN listener is complete"
 fi
 
-if [ -d /opt/kafka/oauth-certs/clienttls ]; then
-  echo "Preparing truststore for OAuth on cienttls listener"
+OAUTH_CERT_DIR="/opt/kafka/certificates/oauth-tls-9093-certs"
+OAUTH_STORE="/tmp/kafka/oauth-tls-9093.truststore.p12"
+if [ -d "$OAUTH_CERT_DIR" ]; then
+  echo "Preparing truststore for OAuth on TLS listener"
+
   # Add each certificate to the trust store
-  STORE=/tmp/kafka/oauth-clienttls.truststore.p12
   declare -i INDEX=0
-  for CRT in /opt/kafka/oauth-certs/clienttls/**/*; do
+  for CRT in $OAUTH_CERT_DIR/**/*; do
     ALIAS="oauth-${INDEX}"
-    echo "Adding $CRT to truststore $STORE with alias $ALIAS"
-    create_truststore "$STORE" "$CERTS_STORE_PASSWORD" "$CRT" "$ALIAS"
+    echo "Adding $CRT to truststore $OAUTH_STORE with alias $ALIAS"
+    create_truststore "$OAUTH_STORE" "$CERTS_STORE_PASSWORD" "$CRT" "$ALIAS"
     INDEX+=1
   done
-  echo "Preparing truststore for OAuth on cienttls listener is complete"
+  echo "Preparing truststore for OAuth on TLS listener is complete"
 fi
 
-if [ -d /opt/kafka/oauth-certs/external ]; then
+OAUTH_CERT_DIR="/opt/kafka/certificates/oauth-external-9094-certs"
+OAUTH_STORE="/tmp/kafka/oauth-external-9094.truststore.p12"
+if [ -d "$OAUTH_CERT_DIR" ]; then
   echo "Preparing truststore for OAuth on external listener"
+
   # Add each certificate to the trust store
-  STORE=/tmp/kafka/oauth-external.truststore.p12
   declare -i INDEX=0
-  for CRT in /opt/kafka/oauth-certs/external/**/*; do
+  for CRT in $OAUTH_CERT_DIR/**/*; do
     ALIAS="oauth-${INDEX}"
-    echo "Adding $CRT to truststore $STORE with alias $ALIAS"
-    create_truststore "$STORE" "$CERTS_STORE_PASSWORD" "$CRT" "$ALIAS"
+    echo "Adding $CRT to truststore $OAUTH_STORE with alias $ALIAS"
+    create_truststore "$OAUTH_STORE" "$CERTS_STORE_PASSWORD" "$CRT" "$ALIAS"
     INDEX+=1
   done
   echo "Preparing truststore for OAuth on external listener is complete"
+fi
+
+AUTHZ_KEYCLOAK_DIR="/opt/kafka/certificates/authz-keycloak-certs"
+AUTHZ_KEYCLOAK_STORE="/tmp/kafka/authz-keycloak.truststore.p12"
+if [ -d "$AUTHZ_KEYCLOAK_DIR" ]; then
+  echo "Preparing truststore for Authorization with Keycloak"
+
+  # Add each certificate to the trust store
+  declare -i INDEX=0
+  for CRT in $AUTHZ_KEYCLOAK_DIR/**/*; do
+    ALIAS="authz-keycloak-${INDEX}"
+    echo "Adding $CRT to truststore $AUTHZ_KEYCLOAK_STORE with alias $ALIAS"
+    create_truststore "$AUTHZ_KEYCLOAK_STORE" "$CERTS_STORE_PASSWORD" "$CRT" "$ALIAS"
+    INDEX+=1
+  done
+  echo "Preparing truststore for Authorization with Keycloak is complete"
 fi

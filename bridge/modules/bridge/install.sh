f74e1946068be0bb4cfd8add131b0fcf5e5860c9
Paolo Patierno <ppatierno@live.com>
2020-02-11 15:45:30 +0100

    Removed cct_module (#148)
    Added packages installation (previously got by the "user" module)
    Added strimzi 1001 user

--- bridge/modules/bridge/install.sh
+++ bridge/modules/bridge/install.sh
@@ -5,6 +5,10 @@ SOURCES_DIR=/tmp/artifacts
 LICENSE_DIR=/root/licenses
 PRODUCT_LICENSE_DIR=${LICENSE_DIR}/${COM_REDHAT_COMPONENT}
 
+# Add strimzi user with UID 1001
+# The user is in the group 0 to have access to the mounted volumes and storage
+useradd -r -m -u 1001 -g 0 strimzi
+
 # untar the artifact containing license information
 mkdir -p ${PRODUCT_LICENSE_DIR}
 mv ${SOURCES_DIR}/kafka-bridge-licenses.tar.gz ${LICENSE_DIR}

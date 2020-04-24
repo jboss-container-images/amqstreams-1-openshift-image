ac8657993b3258c5848416ec43b5c4a5aba44d96
Paolo Patierno <ppatierno@live.com>
2020-04-24 18:57:54 +0200

    Moved to new bundle image format for manifests/metadata

--- common/modules/olm/install.sh
+++ common/modules/olm/install.sh
@@ -6,6 +6,10 @@ MANIFESTS_DIR=${SCRIPT_DIR}/manifests
 
 # create destination folder of manifests
 mkdir -p /manifests
+# create destination folder of metadata
+mkdir -p /metadata
 
 # copy manifests files
-cp -r ${MANIFESTS_DIR}/* /manifests
+cp -r ${MANIFESTS_DIR}/*.yaml /manifests
+# copy metadata file
+cp -r ${MANIFESTS_DIR}/metadata/* /metadata

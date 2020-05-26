948f3578f4fc0dcfe700cfa0542415da4ae35e5d
Paolo Patierno <ppatierno@live.com>
2020-05-26 16:48:50 +0200

    Added unpacking and installation of Cruise Control in the Kafka images

--- kafka/modules/kafka/2.4.0/install.sh
+++ kafka/modules/kafka/2.4.0/install.sh
@@ -6,7 +6,7 @@ SOURCES_DIR=/tmp/artifacts
 LICENSE_DIR=/root/licenses
 PRODUCT_LICENSE_DIR=${LICENSE_DIR}/${COM_REDHAT_COMPONENT}
 
-# Copy contents of zip without the root dir
+# Copy contents of AMQ Streams/Kafka zip without the root dir
 TMP=$(zipinfo -1  ${SOURCES_DIR}/streams-ocp-*.zip | grep -oE '^[^/]+' | uniq)
 unzip ${SOURCES_DIR}/streams-ocp-*.zip
 mv ${TMP}/* ${KAFKA_HOME}/
@@ -15,3 +15,8 @@ cp -r ${KAFKA_HOME}/bin/kafka_exporter ${KAFKA_EXPORTER_HOME}/
 
 mkdir -p ${PRODUCT_LICENSE_DIR}
 cp ${KAFKA_HOME}/docs/licen*/* ${PRODUCT_LICENSE_DIR}
+
+# Copy contents of Cruise Control zip without the root dir
+TMP=$(zipinfo -1  ${SOURCES_DIR}/cruise-control-ocp-*.zip | grep -oE '^[^/]+' | uniq)
+unzip ${SOURCES_DIR}/cruise-control-ocp-*.zip
+mv ${TMP}/* ${CRUISE_CONTROL_HOME}/libs/

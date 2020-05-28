c365041f796c61b113438ce32419a25e18eb3826
Kyle Liberti <kliberti@redhat.com>
2020-05-28 16:24:36 -0400

    Revert olm install script to old manifest format
    Signed-off-by: Kyle Liberti <kliberti@redhat.com>

--- common/modules/olm/install.sh
+++ common/modules/olm/install.sh
@@ -6,10 +6,6 @@ MANIFESTS_DIR=${SCRIPT_DIR}/manifests
 
 # create destination folder of manifests
 mkdir -p /manifests
-# create destination folder of metadata
-mkdir -p /metadata
 
 # copy manifests files
 cp -r ${MANIFESTS_DIR}/*.yaml /manifests
-# copy metadata file
-cp -r ${MANIFESTS_DIR}/metadata/* /metadata

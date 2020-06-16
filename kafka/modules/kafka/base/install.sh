60b24bdd976db72dd0d0bda73d705cdcd572b657
Kyle Liberti <kliberti@redhat.com>
2020-06-16 16:40:59 -0400

    Adding tini package
    Signed-off-by: Kyle Liberti <kliberti@redhat.com>

--- kafka/modules/kafka/base/install.sh
+++ kafka/modules/kafka/base/install.sh
@@ -27,15 +27,6 @@ mv ${TMP}/* ${CRUISE_CONTROL_HOME}/
 # extract all the Kafka related scripts
 unzip ${SOURCES_DIR}/strimzi-kafka-scripts.zip -d ${SCRIPTS_DIR}
 
-# patch to remove "tini"
-FILES=$(find ${SCRIPTS_DIR} -type f -name "*_run.sh")
-for f in $FILES
-do
-  sed -i 's/\/usr\/bin\/tini -w -e 143 -- sh -c "${KAFKA_HOME}\/bin\/connect-distributed.sh \/tmp\/strimzi-connect.properties"/${KAFKA_HOME}\/bin\/connect-distributed.sh \/tmp\/strimzi-connect.properties/' $f
-  sed -i 's/\/usr\/bin\/tini -w -e 143 -- sh -c "${KAFKA_HOME}\/bin\/kafka-server-start.sh \/tmp\/strimzi.properties"/${KAFKA_HOME}\/bin\/kafka-server-start.sh \/tmp\/strimzi.properties/' $f
-  sed -i 's/\/usr\/bin\/tini -w -e 143 -- sh -c "${KAFKA_HOME}\/bin\/zookeeper-server-start.sh \/tmp\/zookeeper.properties"/${KAFKA_HOME}\/bin\/zookeeper-server-start.sh \/tmp\/zookeeper.properties/' $f
-done
-
 # NOTE: kafka folder alredy contains the s2i (so no need for a specific cp command)
 cp -r ${SCRIPTS_DIR}/kafka/* ${KAFKA_HOME}/
 chmod -R 755 ${KAFKA_HOME}

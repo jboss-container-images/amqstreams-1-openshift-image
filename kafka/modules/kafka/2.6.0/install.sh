a069d17d45c2bafd809b2527ff18dade45462a56
Kyle Liberti <kliberti@redhat.com>
2020-11-04 12:52:24 -0500

    Update MD5s for AMQ Streams 1.6.0 CR2
    Signed-off-by: Kyle Liberti <kliberti@redhat.com>

--- kafka/modules/kafka/2.6.0/install.sh
+++ kafka/modules/kafka/2.6.0/install.sh
@@ -14,7 +14,7 @@ mv ${TMP}/* ${KAFKA_HOME}/
 cp -r ${KAFKA_HOME}/bin/kafka_exporter ${KAFKA_EXPORTER_HOME}/
 
 mkdir -p ${PRODUCT_LICENSE_DIR}/kafka
-cp ${KAFKA_HOME}/docs/licen*/* ${PRODUCT_LICENSE_DIR}/kafka
+cp -r ${KAFKA_HOME}/licen*/* ${PRODUCT_LICENSE_DIR}/kafka
 
 mkdir -p ${PRODUCT_LICENSE_DIR}/cruise-control
 cp -r ${CRUISE_CONTROL_HOME}/docs/licen*/* ${PRODUCT_LICENSE_DIR}/cruise-control

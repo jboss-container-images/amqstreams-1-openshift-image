353761697fc44f09540d5176fdcfb189199f3c97
Paolo Patierno <ppatierno@live.com>
2021-03-08 12:45:09 +0100

    Updated to 1.7.0 on images and modules Added Kafka 2.7.0

---
+++ kafka/modules/kafka/2.7.0/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+SOURCES_DIR=/tmp/artifacts
+LICENSE_DIR=/root/licenses
+PRODUCT_LICENSE_DIR=${LICENSE_DIR}/${COM_REDHAT_COMPONENT}
+
+# Copy contents of zip without the root dir
+TMP=$(zipinfo -1  ${SOURCES_DIR}/streams-ocp-*.zip | grep -oE '^[^/]+' | uniq)
+unzip ${SOURCES_DIR}/streams-ocp-*.zip
+mv ${TMP}/* ${KAFKA_HOME}/
+
+cp -r ${KAFKA_HOME}/bin/kafka_exporter ${KAFKA_EXPORTER_HOME}/
+
+mkdir -p ${PRODUCT_LICENSE_DIR}/kafka
+cp -r ${KAFKA_HOME}/licen*/* ${PRODUCT_LICENSE_DIR}/kafka
+
+mkdir -p ${PRODUCT_LICENSE_DIR}/cruise-control
+cp -r ${CRUISE_CONTROL_HOME}/docs/licen*/* ${PRODUCT_LICENSE_DIR}/cruise-control

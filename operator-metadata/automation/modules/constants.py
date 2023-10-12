a1afde9fbff659e3205423d229436037ce3701e6
Kyle Liberti <kliberti@redhat.com>
2023-10-12 08:33:08 -0400

    Add bundle automation (#429)
    * Add bundle automation
    
    Signed-off-by: Kyle Liberti <kliberti@redhat.com>
    
    * Add extra_dir so files are copied to dist-git
    
    ---------
    
    Signed-off-by: Kyle Liberti <kliberti@redhat.com>

---
+++ operator-metadata/automation/modules/constants.py
@@ -0,0 +1,9 @@
+RELEASE_TAG = "rhaos-middleware-rhel-8-container-released"
+OPERATOR_PACKAGE_NAME = "amqstreams-operator-container"
+METADATA_PACKAGE_NAME = "amqstreams-bundle-container"
+PACKAGE_NAME_SUFFIX = "-container"
+STRIMZI_MAVEN_BUILDER = "strimzi-maven-builder"
+RELEASED = False
+COMPLETED = 1
+OLD_BUNDLE_VERSION_INDEX=0
+NEW_BUNDLE_VERSION_INDEX=1

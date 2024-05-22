41af73adf03049e70a5baa55e1c3b8ef9bec5090
Kyle Liberti <kliberti@redhat.com>
2024-05-22 09:44:12 -0400

    Add new Maven Builder image (#484)
    Signed-off-by: Kyle Liberti <kliberti@redhat.com>

--- operator-metadata/automation/modules/constants.py
+++ operator-metadata/automation/modules/constants.py
@@ -1,8 +1,8 @@
-RELEASE_TAG = "rhaos-middleware-rhel-8-container-released"
+RELEASE_TAG = "rhaos-middleware-rhel-9-container-released"
 OPERATOR_PACKAGE_NAME = "amqstreams-operator-container"
+MAVEN_BUILDER_PACKAGE_NAME = "amqstreams-maven-builder"
 METADATA_PACKAGE_NAME = "amqstreams-bundle-container"
 PACKAGE_NAME_SUFFIX = "-container"
-STRIMZI_MAVEN_BUILDER = "strimzi-maven-builder"
 RELEASED = False
 COMPLETED = 1
 OLD_BUNDLE_VERSION_INDEX=0
